6b37323dc728256220d9491b77b759e5bf1be2e0
Roberto <Roberto@ROBERTO>
2018-05-08 15:26:37 +0200

    Aggiungere i file di progetto.

---
+++ Tesi/Tesi.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Tesi.ino
+ Created:	08/05/2018 15:26:20
+ Author:	Roberto
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
